2ce4b3a6dc03e944aba90bf73a4562cac45109d2
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2018-02-09 15:35:49 -0600

    Refactor tab symbolTable.h

--- symtab/symbolTable.h
+++ symtab/symbolTable.h
@@ -13,10 +13,10 @@
 
 enum TypeKind {VOID, INT, ARRAY, FUNCTION};
 
-struct type{
-	enum TypeKind kind;   /* one from the enum above */
-	int  dimension; /* for arrays */
-	struct type *function; /*function argument and return types */
+struct type {
+    enum TypeKind kind;   /* one from the enum above */
+    int  dimension; /* for arrays */
+    struct type *function; /*function argument and return types */
 };
 
 typedef struct type Type;
@@ -24,42 +24,43 @@ typedef struct type Type;
 typedef struct type *TypePtr;
         
 struct element {
-	int		key;
-    char *		id;	       
-	int             linenumber;
-	int             scope;   /* scope depth at declaration */
-	TypePtr	stype;	      /* pointer to the type infomation */
-	void *ast;           // AST information, skip for this homework
-	struct element	*next;		/* pointer to the next symbol with the same hash table index */
+    int     key;
+    char *      id;        
+    int             linenumber;
+    int             scope;   /* scope depth at declaration */
+    TypePtr stype;        /* pointer to the type infomation */
+    void *ast;           // AST information, skip for this homework
+    struct element  *next;      /* pointer to the next symbol with the same hash table index */
 };
 
 typedef struct element Element;
 
-typedef struct element	*ElementPtr;
+typedef struct element  *ElementPtr;
 
 typedef ElementPtr HashTableEntry;
 
 /* data structure of the symbol table */
 
 struct symbolTable {
-	HashTableEntry	hashTable[MAXHASHSIZE];		/* hash table  */
+    HashTableEntry  hashTable[MAXHASHSIZE];     /* hash table  */
 };
 
 typedef struct symbolTable *SymbolTablePtr;
 
 struct symbolTableStackEntry {
-	SymbolTablePtr			symbolTablePtr;
-	struct symbolTableStackEntry	*prevScope; //previous scope
+    SymbolTablePtr          symbolTablePtr;
+    struct symbolTableStackEntry    *prevScope; //previous scope
 } SymbolTableStackEntry;
 
 typedef struct symbolTableStackEntry *SymbolTableStackEntryPtr;
 
 
-int		    initSymbolTable() ;
-ElementPtr		symLookup(char *);
-ElementPtr		symInsert(char *, struct type * , int );
-int			enterScope();
-void			leaveScope();
+int         initSymbolTable() ;
+ElementPtr  symLookup(char *);
+ElementPtr  symInsert(char *, struct type * , int );
+int         enterScope();
+void        leaveScope();
+int         hash(char *, int );
 
 
 #endif
