606641e030df75fa6beda014a32b615d04fbbbb0
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2018-02-11 11:42:57 -0600

    Reverse printElement to original version for submission purposes

--- symtab/src/symbolTable.c
+++ symtab/src/symbolTable.c
@@ -133,14 +133,9 @@ void leaveScope() {
 // Do not modify this function
 void printElement(ElementPtr symelement) {
     if (symelement != NULL) {
-        printf(
-            "Line %d: %s, scopeDepth: %d\n",
-            symelement->linenumber,
-            symelement->id,
-            symelement->scope
-        );
+        printf("Line %d: %s", symelement->linenumber, symelement->id);
     }
-    else printf("Wrong call! symbol table entry NULL\n");
+    else printf("Wrong call! symbol table entry NULL");
 }
 
 
