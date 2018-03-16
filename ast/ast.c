ae9ecdf927a8b9b26493936d1185c7456ae92a13
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2018-03-15 22:21:08 -0500

    Add NULL assignment to children 1 and 2

--- ast/ast.c
+++ ast/ast.c
@@ -13,6 +13,8 @@ AstNodePtr new_ExprNode(ExpKind kind, int nLinenumber)
     node->nKind = EXPRESSION;
     node->eKind = kind;
     node->children[0] = NULL;
+    node->children[1] = NULL;
+    node->children[2] = NULL;
     node->sibling = NULL;
     node->nSymbolTabPtr = symbolStackTop->symbolTablePtr;
     node->nLinenumber = nLinenumber;
@@ -27,6 +29,8 @@ AstNodePtr new_StmtNode(StmtKind kind, int nLinenumber)
     node->nKind = STMT;
     node->sKind = kind;
     node->children[0] = NULL;
+    node->children[1] = NULL;
+    node->children[2] = NULL;
     node->sibling = NULL;
     node->nSymbolTabPtr = symbolStackTop->symbolTablePtr;
     node->nLinenumber = nLinenumber;
@@ -40,6 +44,8 @@ AstNodePtr new_Node(NodeKind kind, char* name, int nLinenumber)
     AstNodePtr node = (AstNode *)malloc(sizeof(AstNode));
     node->nKind = kind;
     node->children[0] = NULL;
+    node->children[1] = NULL;
+    node->children[2] = NULL;
     node->sibling = NULL;
     node->nSymbolPtr = symLookup(name);
     node->nSymbolTabPtr = symbolStackTop->symbolTablePtr;
