a6345f4777a0d36726f98d8aa5950b22683491ea
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2018-04-08 18:35:20 -0500

    Add additional tests

---
+++ typecheck/tests/micro/bad/test4.c
@@ -0,0 +1,6 @@
+int a(void){
+	if(1) {
+		return 1;
+	}
+}
+		
---
+++ typecheck/tests/micro/bad/test5.c
@@ -0,0 +1,40 @@
+void a(void)
+{
+	if (1)
+	{
+		return;
+	}
+}
+
+void main(void){
+	int b[10];
+	b[1] = 3;
+	while(b){
+		return;
+	}
+	while(1){
+		if(b[1]){
+			return 1;
+		}
+	}
+	output(3);
+	b[2] = input();
+	a(b[3]);
+	b[2] = input(b[3]);	
+	func();
+
+}
+
+int g(void){
+	if(1){
+		return 1;
+	}
+	return 1;
+}
+
+int h(void){
+	if(1){
+		return 1;
+	}
+}
+
