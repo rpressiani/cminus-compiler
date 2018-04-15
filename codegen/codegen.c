a21dcf2a8f45cb85ee89a3bc725299ee2905f58e
Riccardo Pressiani <riccardo.pressiani@gmail.com>
2018-04-15 13:22:09 -0500

    Add local var declaration

--- codegen/codegen.c
+++ codegen/codegen.c
@@ -21,13 +21,60 @@ void code_gen_expr(AstNode *node){
 ;
 }
 
-void code_gen_stmt(AstNode *node){
-;
+void code_gen_localVarDecl(SymbolTablePtr scope) {
+    int nVar = 0;
+    char *instr;
+
+    for(int i=0; i<MAXHASHSIZE; i++) {
+        ElementPtr symelement = scope->hashTable[i];
+        while(symelement) {
+            switch(symelement->stype->kind) {
+                case INT:
+                    nVar++;
+                    break;
+                case ARRAY:
+                    // TODO
+                    break;
+                // case VOID:
+                //     break;
+                // case FUNCTION:
+                //     break;
+                default:
+                // TODO
+                    printf("ERROR\n");
+                    break;
+            }
+            symelement = symelement->next; 
+        }
+    }
+
+    asprintf(&instr, "subu $sp, $sp, %d", nVar*4);
+    emit(instr);
 }
 
-void code_gen_method(AstNode *node){
-;
+void code_gen_stmt(AstNode *stmt){
+    if(stmt == NULL) return;
+
+    switch(stmt->sKind) {
+        case IF_THEN_ELSE_STMT:
+            break;
+        case WHILE_STMT:
+            break;
+        case RETURN_STMT:
+            break;
+        case COMPOUND_STMT:
+            if(stmt->nSymbolTabPtr != NULL) code_gen_localVarDecl(stmt->nSymbolTabPtr);
+            // Print the first statement
+            if(stmt->children[0] != NULL) code_gen_stmt(stmt->children[0]);
+            break;
+        case EXPRESSION_STMT:
+            break;
+    }
 }
+
+// void code_gen_method(AstNode *node){
+// ;
+// }
 
 void printFile(char* filename) {
     FILE *fptr = fopen(filename, "r");
@@ -52,10 +99,18 @@ void codegen_helper(AstNode *root) {
     switch(root->nKind) {
         case METHOD:
             emit_label(root->nSymbolPtr->id);
+            emit("subu $sp, $sp, 8");
+            emit("sw $fp, 4($sp)");
+            emit("sw $ra, 0($sp)");
+            emit("addiu $fp, $sp, 4");
+
+            codegen_helper(root->children[1]); // body of the method
+
             break;
         case FORMALVAR:
             break;
         case STMT:
+            code_gen_stmt(root);
             break;
         case EXPRESSION:
             break;
@@ -65,7 +120,7 @@ void codegen_helper(AstNode *root) {
 void codegen(){
 
     if (program) {
-        outfile = fopen( "test.s", "w+" );
+        outfile = fopen("temp.s","w+");
 
         emit(".text");
         emit(".align 2");
@@ -77,14 +132,6 @@ void codegen(){
     } else {
         printf("ERROR\n");
     }
-
-    if (VERBOSE) printFile("test.s");
-
     
-    // emit("subu $sp, $sp, 8");
-    // emit("sw $fp, 4($sp)");
-    // emit("sw $ra, 0($sp)");
-    // emit("addiu $fp, $sp, 4");
- /* from now on call code gen on the main stmt in main */
-
-}   
+    if (VERBOSE) printFile("temp.s");
+}
--- codegen/codegen.h
+++ codegen/codegen.h
@@ -1,8 +1,2 @@
-void emit(const char *);
-char *gen_new_label();
-int gen_offsets(SymbolTablePtr s);
-void code_gen_expr(AstNode *);
-void code_gen_stmt(AstNode *);
-void code_gen_method(AstNode *);
-void code_gen_binary_expr(AstNode *);
+// int gen_offsets(SymbolTablePtr s);
 void codegen();
